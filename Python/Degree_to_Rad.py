7f4ada584089b7c2739d5071ba112ca9605a10db
Ajin k j <61120485+Ajinkj@users.noreply.github.com>
2021-10-13 20:36:47 +0530

    Add files via upload

---
+++ Python/Degree_to_Rad.py
@@ -0,0 +1,13 @@
+# pi=22/7
+# degree = float(input("Input degrees: "))
+# radian = degree*(pi/180)
+# print('Radian is, %0.6f ' %(radian))
+pi=3.14
+print('value:',pi,'type:',type(pi))
+
+i=2
+print(float(i))
+
+x="Hello"
+y='World'
+print(x+' '+y)
