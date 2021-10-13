1fe5683ceed339ce2a576fa0d989886340a3bd17
amruthasures <85220121+amruthasures@users.noreply.github.com>
2021-10-13 11:25:09 +0530

    Add files via upload

---
+++ JAVA/arithematic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class arithematic
+{
+  public static int main(String[]args)
+  {
+   sum obj=new sum(6,8);
+     obj.add();
+   }
+}
+class sum
+{ 
+  public void add(int a, int b);
+
+  int c=a+b;
+System.out.println(c);
+}
