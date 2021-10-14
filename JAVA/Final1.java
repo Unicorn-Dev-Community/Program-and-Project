94eba5bab8476338f85a40757b6e4ec522d9d661
AISWARYA K <66054986+AISWARYAK99@users.noreply.github.com>
2021-10-14 11:32:33 +0530

    Final Keyword in java.

---
+++ JAVA/Final1.java
@@ -0,0 +1,9 @@
+class Final1
+{
+	public static void main(String[] args)
+	{
+		final int x=20;
+		System.out.println(x);
+		//x=x++;//this is an error we cant change a variable of final keyword
+	}
+}
