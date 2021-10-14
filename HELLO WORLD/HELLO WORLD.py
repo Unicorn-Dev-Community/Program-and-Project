4aa21459c3d2450867a73670f432f6da705c4549
Alfred Francis <60069653+alfredfrancis68@users.noreply.github.com>
2021-10-14 16:18:45 +0530

    HELLO WORLD

---
+++ HELLO WORLD/HELLO WORLD.py
@@ -0,0 +1,363 @@
+class\
+W0rStHeLloWoRlD\
+    :
+    def\
+    __init__\
+    (\
+    self\
+    ):
+        self\
+            .\
+        ArrAy\
+            =\
+                []
+        
+        self\
+            .\
+            ArrAy_Add\
+            (\
+            )
+
+    def\
+        ArrAy_Add\
+            (\
+                self\
+                )\
+                :
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    H\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    e\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    l\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    l\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    o\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    -\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    W\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    o\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    r\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    l\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+                self\
+                    .\
+                    ArrAy\
+                        .\
+                        append\
+                            (\
+                                '\
+                                    d\
+                                        '.\
+                                            strip\
+                                                (\
+                                                    ' '\
+                                                        )\
+                                                            )
+
+                h\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        0\
+                                            ]\
+                                                )
+                e\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        1\
+                                            ]\
+                                                )
+                l\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        2\
+                                            ]\
+                                                )
+                ll\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        3\
+                                            ]\
+                                                )
+                o\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        4\
+                                            ]\
+                                                )
+                x\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        5\
+                                            ]\
+                                                )
+                w\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        6\
+                                            ]\
+                                                )
+                o\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        7\
+                                            ]\
+                                                )
+                r\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        8\
+                                            ]\
+                                                )
+                lx\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        9\
+                                            ]\
+                                                )
+                d\
+                    =\
+                    (\
+                        self\
+                            .\
+                                ArrAy\
+                                    [\
+                                        10\
+                                            ]\
+                                                )
+                strinG = \
+                    (\
+                    "{}{}{}{}{} {}{}{}{}{}"\
+                        .\
+                            format\
+                                (\
+                                    h\
+                                        ,\
+                                            e\
+                                                ,\
+                                                    l\
+                                                        ,\
+                                                            ll,\
+                                                                o,\
+                                                                    w,\
+                                                                        o,\
+                                                                            r,\
+                                                                                lx,\
+                                                                                    d\
+                                                                                        )\
+                                                                                            )
+                
+                print\
+                    (\
+                        strinG\
+                            .\
+                                strip\
+                                    (\
+                                        ' '\
+                                            )\
+                                                )
+              
+
+
+if\
+    __name__\
+        ==\
+            '__main__'\
+                :
+                ClAssVaRiable\
+                    =\
+                    W0rStHeLloWoRlD\
+                    ()
+   
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
