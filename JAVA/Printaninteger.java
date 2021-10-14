794170fe28944f9ad865d101741d35d27bc11fa8
Aneena Antony <73019565+Aneenaanu321@users.noreply.github.com>
2021-10-14 07:09:34 +0530

    Create Printaninteger.java

---
+++ JAVA/Printaninteger.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number :") ;
+        int number = reader.nextInt();
+        System.out.println("You entered: " + number);
+    }
+}
